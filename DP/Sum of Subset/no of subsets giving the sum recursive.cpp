3963eda049e53c51262dfa6d822198ff21b3fc6d
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2019-08-27 12:46:08 +0530

    Create no of subsets giving the sum recursive.cpp

---
+++ DP/Sum of Subset/no of subsets giving the sum recursive.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int SubsetSum(int a[],int n,int sum){
+	//base condition;
+	if(sum==0)
+	return 1;
+	else if(sum<0)
+	return 0;
+	else if(sum>0 && n<=0)
+	return 0;
+	else 
+	return (SubsetSum(a,n-1,sum)+SubsetSum(a,n-1,sum-a[n-1]));
+}
+
+int main() {
+	// your code goes here
+	int n,sum;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+	cin>>a[i];
+	cin>>sum;
+	int ans=SubsetSum(a,n,sum);
+	if(ans)
+	cout<<"Yes "<<ans;
+	else
+	cout<<"No";
+	return 0;
+}
