5bfd3743ac7df9f6404fcb7ff0ae05e5a3c95c84
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2019-09-23 16:05:44 +0530

    Create top view.cpp

---
+++ Trees/top view.cpp
@@ -0,0 +1,24 @@
+void topView(struct Node *root)
+{
+    //Your code here
+    if(root==NULL)
+    return;
+    queue<pair<Node *,int>>q;
+    map<int,int>m;
+    q.push(make_pair(root,0));
+    while(!q.empty()){
+        pair<Node *,int>p=q.front();
+        Node *temp=p.first;
+        int d=p.second;
+        q.pop();
+        if(m.find(d)==m.end())
+        m[d]=temp->data;
+        if(temp->left)
+        q.push(make_pair(temp->left,d-1));
+        if(temp->right)
+        q.push(make_pair(temp->right,d+1));
+    }
+    map<int,int>::iterator it;
+    for(it=m.begin();it!=m.end();it++)
+    cout<<it->second<<" ";
+}
